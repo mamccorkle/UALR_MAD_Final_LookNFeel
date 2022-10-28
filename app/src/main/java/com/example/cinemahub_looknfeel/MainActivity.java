97e128ab34f80a83b10c1a5273325ed22906f02c
Mark McCorkle <mamccorkle@ualr.edu>
2022-10-28 14:40:43 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/cinemahub_looknfeel/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.cinemahub_looknfeel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.acitvity_register);
+//        setContentView(R.layout.activity_login);
+//        setContentView(R.layout.activity_theater_selection);
+//        setContentView(R.layout.activity_theater_selection_v2);
+//        setContentView(R.layout.activity_payment);
+//        setContentView(R.layout.activity_seating_chart);
+//        setContentView(R.layout.activity_main_page);
+        setContentView(R.layout.activity_main_page_v2);
+    }
+}
