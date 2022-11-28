dcfd77dbe5216fc14d93ed9305b0e2703788e2a3
Mark McCorkle <mamccorkle@ualr.edu>
2022-11-27 19:14:28 -0600

    Commit - 202211271914

--- app/src/main/java/com/example/cinemahub_looknfeel/MainActivity.java
+++ app/src/main/java/com/example/cinemahub_looknfeel/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.cinemahub_looknfeel;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +19,35 @@ public class MainActivity extends AppCompatActivity {
 //        setContentView(R.layout.activity_payment);
 //        setContentView(R.layout.activity_seating_chart);
 //        setContentView(R.layout.activity_main_page);
-        setContentView(R.layout.activity_main_page_v2);
+//        setContentView(R.layout.activity_main_page_v2);
+//        setContentView(R.layout.activity_seating_chart_v2);
+        setContentView(R.layout.activity_seating_chart_v3);
+
+
+
+        // Actionbar Set Up: ///////////////////////////////////////////////////////////////////////
+
+        // calling the action bar
+        ActionBar actionBar = getSupportActionBar();
+
+        // showing the back button in action bar
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        ////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+        // Spinner Set Up: /////////////////////////////////////////////////////////////////////////
+
+        Spinner spinner = (Spinner) findViewById(R.id.seatSpinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.seats_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+        ////////////////////////////////////////////////////////////////////////////////////////////
+
     }
 }
